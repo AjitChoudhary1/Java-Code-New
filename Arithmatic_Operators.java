414f48ecd08feea24d18428c265d5e6510be841f
Ajit Choudhary <ajitchoudhary8764@gmail.com>
2025-04-04 19:58:31 +0530

    core java new code

---
+++ Arithmatic_Operators.java
@@ -0,0 +1,51 @@
+package core_java_new;
+public class Arithmatic_Operators 
+{
+	static void add()
+	{
+		int a=100;
+		int b=110;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+	static void sub()
+	{
+		int a=100;
+		int b=110;
+		int sub=a-b;
+		System.out.println(sub);
+	}
+	static void multi()
+	{
+		int a=10;
+		int b=11;
+		int mul=a*b;
+		System.out.println(mul);
+	}
+	static void divide()
+	{
+		int a=30;
+		int b=10;
+		int div=a/b;
+		System.out.println(div);
+	}
+	static void modu()
+	{
+		int a=25;
+		int b=15;
+		int mod=a%b;
+		System.out.println(mod);
+	}
+	
+	public static void main(String[] args) 
+	{
+		add();
+		sub();
+		multi();
+		divide();
+		modu();
+		
+		
+	}
+
+}
